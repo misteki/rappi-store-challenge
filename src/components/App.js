38104f7cddfd7eb7dfcf2bf6df79b3eb54ffe01b
Matias <mcincunegui.dev@gmail.com>
2019-06-08 12:05:47 -0300

    Add and configure linting, format JS files according to linet configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
 import './App.css';
-import {Navbar} from './navbar/Navbar';
+import Navbar from './navbar/Navbar';
 
-function App() {
-  return (
-    <div className="app">
-      < Navbar />
-      <section className="app-main" role="main">
-        <ul className="product-list">
-        </ul>
-      </section>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <section className="app-main" role="main">
+      <ul className="product-list" />
+    </section>
+  </div>
+);
 
 export default App;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import {Target} from 'react-feather';
+import { Target } from 'react-feather';
 
 import './Navbar.css';
 
-export const Navbar = () => {
+const Navbar = () => {
   const title = 'El Baratón';
 
   return (
     <header className="navbar">
       <Target className="navbar-icon" size="2.8em" />
-      <h1 className="navbar-title"> {title} </h1>
-  </header>
-);
-}
+      <h1 className="navbar-title">
+        {' '}
+        {title}
+        {' '}
+      </h1>
+    </header>
+  );
+};
+
+export default Navbar;
