9b330d32cc3a6d9d80b039b28803d8003416cba0
Matias <mcincunegui.dev@gmail.com>
2019-06-08 17:16:22 -0300

    Fetch data from JSON using fake async service methods

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,29 @@ import Navbar from './navbar/Navbar';
 import Sidebar from './sidebar/Sidebar';
 import Products from './products/Products';
 
-const App = () => (
-  <div className="app">
-    <Navbar />
-    <main className="app-main">
-      <Sidebar />
-      <Products />
-    </main>
-  </div>
-);
+import { getProducts, getCategories } from '../services/store-service';
+
+const App = () => {
+  let products = [];
+  let categories = [];
+
+  getProducts().then((data) => {
+    products = data;
+  });
+
+  getCategories().then((data) => {
+    categories = data;
+  });
+
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="app-main">
+        <Sidebar />
+        <Products />
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/services/store-service.js
+++ src/services/store-service.js
@@ -1,7 +1,10 @@
-export const getProducts = () => {
+import * as PRODUCTS_DATA from './data/products.json';
+import * as CATEGORIES_DATA from './data/categories.json';
 
-};
+export const getProducts = async () => new Promise((resolve) => {
+  resolve(PRODUCTS_DATA.products);
+});
 
-export const getCategories = () => {
-
-};
+export const getCategories = async () => new Promise((resolve) => {
+  resolve(CATEGORIES_DATA.categories);
+});
