9adfb785ec4e438b55ff30efbc0ca79e0f1aa57b
Matias <mcincunegui.dev@gmail.com>
2019-06-12 19:08:38 -0300

    Complete paginator

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -5,27 +5,45 @@ import Paginator from './paginator/Paginator';
 
 import './Products.css';
 
-const Products = (props) => {
-  const { products, selectedCategory } = props;
+class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageSize: 9,
+      currentPage: 0,
+    };
+    this.onPageChange = this.onPageChange.bind(this);
+  }
 
-  const getSubcategoriesIDs = (category) => {
-    let ids = [];
-    if (category.sublevels) {
-      ids = category.sublevels.map(subcategory => getSubcategoriesIDs(subcategory));
-    }
-    return [category.id, ...ids.flat()];
-  };
-  const selectedCategoriesIDs = selectedCategory ? getSubcategoriesIDs(selectedCategory) : [];
+  onPageChange(page) {
+    this.setState({ currentPage: page });
+  }
 
-  const filteredProducts = products
-    .filter(product => !selectedCategory || selectedCategoriesIDs.includes(product.sublevel_id))
-    .slice(0, 9);
+  render() {
+    const { products, selectedCategory } = this.props;
+    const { pageSize, currentPage } = this.state;
 
-  return (
-    <React.Fragment>
-      <ul className="product-list">
-        {
-          filteredProducts.map(product => (
+    const getSubcategoriesIDs = (category) => {
+      let ids = [];
+      if (category.sublevels) {
+        ids = category.sublevels.map(subcategory => getSubcategoriesIDs(subcategory));
+      }
+      return [category.id, ...ids.flat()];
+    };
+    const selectedCategoriesIDs = selectedCategory ? getSubcategoriesIDs(selectedCategory) : [];
+
+    const filteredProducts = products
+      .filter(product => !selectedCategory || selectedCategoriesIDs.includes(product.sublevel_id));
+
+    const firstProductInPageIndex = pageSize * currentPage;
+    const lastProductInPageIndex = firstProductInPageIndex + pageSize;
+    const productsInPage = filteredProducts.slice(firstProductInPageIndex, lastProductInPageIndex);
+
+    return (
+      <React.Fragment>
+        <ul className="product-list">
+          {
+          productsInPage.map(product => (
             <li className="product-list-item" key={product.id}>
               <h2 className="product-name">{product.name}</h2>
               <h3 className="product-price">{product.price}</h3>
@@ -38,15 +56,16 @@ const Products = (props) => {
             </li>
           ))
         }
-        {
+          {
           filteredProducts.length === 0
           && (<h1 className="no-products"> No products found</h1>)
         }
-      </ul>
-      <Paginator />
-    </React.Fragment>
-  );
-};
+        </ul>
+        {filteredProducts.length > 0 && <Paginator entries={filteredProducts.length} pageSize={pageSize} currentPage={currentPage} onPageChange={this.onPageChange} />}
+      </React.Fragment>
+    );
+  }
+}
 
 Products.propTypes = {
   products: PropTypes.array,
--- src/components/products/paginator/Paginator.js
+++ src/components/products/paginator/Paginator.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import { ArrowRight, ArrowLeft } from 'react-feather';
+import shortid from 'shortid';
 
 import './Paginator.css';
 
-const Paginator = props => (
-  <nav className="paginator-container">
-    <div className="paginator">
-      <ArrowLeft size={16} />
-      <button type="button" className="paginator-button selected"> 1 </button>
-      <button type="button" className="paginator-button"> 2 </button>
-      <button type="button" className="paginator-button"> 3 </button>
-      <button type="button" className="paginator-button"> 4 </button>
-      <button type="button" className="paginator-button"> 5 </button>
-      <ArrowRight size={16} />
+const Paginator = (props) => {
+  const {
+    entries, pageSize, currentPage, onPageChange,
+  } = props;
 
-    </div>
-  </nav>
-);
+  const pages = Math.ceil(entries / pageSize);
+  const firstPage = 0;
+  const lastPage = pages - 1;
+
+  return (
+    <nav className="paginator-container">
+      <div className="paginator">
+        {
+          currentPage > firstPage
+          && (
+          <button type="button" onClick={() => { onPageChange(currentPage - 1); }}>
+            <ArrowLeft size={14} />
+          </button>
+          )
+          }
+        {
+        [...Array(pages)].map((page, pageIndex) => (
+          <button
+            type="button"
+            className={`paginator-button ${pageIndex === currentPage ? 'selected' : ''}`}
+            onClick={() => { onPageChange(pageIndex); }}
+            key={shortid.generate()}
+          >
+            {pageIndex + 1}
+          </button>
+        ))
+      }
+        {
+          currentPage < lastPage
+          && (
+          <button type="button" onClick={() => { onPageChange(currentPage + 1); }}>
+            <ArrowRight size={14} />
+          </button>
+          )
+          }
+      </div>
+    </nav>
+  );
+};
 
 export default Paginator;
